f4c8cfbebde654782f96c01c4e1d2fb640a680c1
menomik1011 <39692905+meno1011@users.noreply.github.com>
2022-08-17 18:16:53 +0900

    kakao login button 부분 필요없는 부분 수정

---
+++ src/service/reduxt-test.js
@@ -0,0 +1,36 @@
+// import { createStore } from "@reduxjs/toolkit";
+import { createStore } from "redux";
+
+// 리듀서 : 액션이 어떻게 상태를 다음 상태로 변경하는지 실행
+// 상태의 모양은 자유이다. 기본형일수도 배열일수도 객체일수도
+//
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+// 앱의 상태를 보관하는 Redux 저장소를 만듭니다.
+// API로는 { subscribe, dispatch, getState }가 있습니다.
+let store = createStore(counter)
+
+// subscribe()를 이용해 상태 변화에 따라 UI가 변경되게 할 수 있습니다.
+// 보통은 subscribe()를 직접 사용하기보다는 뷰 바인딩 라이브러리(예를 들어 React Redux)를 사용합니다.
+// 하지만 현재 상태를 localStorage에 영속적으로 저장할 때도 편리합니다.
+
+store.subscribe(() => console.log(store.getState()))
+
+// 내부 상태를 변경하는 유일한 방법은 액션을 보내는 것뿐입니다.
+// 액션은 직렬화할수도, 로깅할수도, 저장할수도 있으며 나중에 재실행할수도 있습니다.
+store.dispatch({ type: 'INCREMENT' })
+// 1
+store.dispatch({ type: 'INCREMENT' })
+// 2
+store.dispatch({ type: 'DECREMENT' })
+// 1
