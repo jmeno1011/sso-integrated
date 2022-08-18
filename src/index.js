9c7ce408ff982ed8b27e5e71d7a9c1d934f92dbf
menomik1011 <39692905+meno1011@users.noreply.github.com>
2022-08-18 12:40:32 +0900

    react-redux 추가 , store 추가

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import counter from './modules/counter';
+import { Provider } from 'react-redux';
+
+const store = createStore(counter);
 
 ReactDOM.render(
-      <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>
+      ,
   document.getElementById("root")
 );
 
