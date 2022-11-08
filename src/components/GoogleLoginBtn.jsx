cc8edde11cafce759ff66eea0d1cae48f40bd8d0
jmeno1011 <jwhltn8282@gmail.com>
2022-11-08 23:34:33 +0900

    react-google-login delete

--- src/components/GoogleLoginBtn.jsx
+++ src/components/GoogleLoginBtn.jsx
@@ -1,13 +1,5 @@
-import { useEffect } from "react";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
-import { gapi } from "gapi-script";
+import { useEffect, useRef } from "react";
 import styled from "styled-components";
-import { useRef } from "react";
-
-const GoogleLayout = styled.div`
-  padding: 16px;
-  border: 2px solid #333;
-`
 
 const GoogleLoginBtn = ({ user, setUser }) => {
   const googleButtonRef = useRef(null);
@@ -32,57 +24,5 @@ const GoogleLoginBtn = ({ user, setUser }) => {
     <div ref={googleButtonRef}/>
   )
 }
-
-// const GoogleLoginBtn = ({ user, setUser }) => {
-//   useEffect(() => {
-//     const initClient = () => {
-//       gapi.client.init({
-//         clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-//         scope: "",
-//       });
-//     };
-//     gapi.load("client:auth2", initClient);
-//   });
-
-//   const onSuccess = (res) => {
-//     setUser(res);
-//     console.log("success:", res);
-//   };
-
-//   const onFailure = (err) => {
-//     console.log("failed:", err);
-//   };
-
-//   const logout = (res) => {
-//     console.log(res);
-//     setUser({});
-//   };
-
-//   return (
-//     <>
-//       {Object.keys(user).length === 0 ? (
-//         // <GoogleLayout>
-//           <GoogleLogin
-//             clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-//             onSuccess={onSuccess}
-//             onFailure={onFailure}
-//             cookiePolicy={"single_host_origin"}
-//             isSignedIn={true}
-//           />
-//         // </GoogleLayout>
-//       ) : (
-//         <div style={{ display: "flex", flexDirection: "column" }}>
-//           <img src={user.profileObj.imageUrl} alt="profile" />
-//           <h3>{user.profileObj.name}</h3>
-//           <GoogleLogout
-//             clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-//             buttonText="Logout"
-//             onLogoutSuccess={logout}
-//           ></GoogleLogout>
-//         </div>
-//       )}
-//     </>
-//   );
-// };
 
 export default GoogleLoginBtn;
