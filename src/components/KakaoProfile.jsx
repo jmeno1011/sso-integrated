3f52543783783a5d027f985938020f437d58628e
jmeno1011 <jwhltn8282@gmail.com>
2023-02-01 23:03:48 +0900

    add isInitialized

--- src/components/KakaoProfile.jsx
+++ src/components/KakaoProfile.jsx
@@ -2,6 +2,10 @@ import { useEffect } from "react";
 
 const KakaoProfile = () => {
   const getProfile = async () => {
+    if (!window.Kakao.isInitialized()) {
+      window.Kakao.init(process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY);
+      console.log(window.Kakao.isInitialized());
+    }
     try {
       let data = await window.Kakao.API.request({ url: "/v2/user/me" });
       console.log(data);
